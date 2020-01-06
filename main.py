20b92b17de7b40b57422bb1b815537c0f861c9a6
Jo Hyuk Jun <johyukjun@Jo-MacBookPro.local>
2020-01-06 23:22:37 +0900

    server update

--- main.py
+++ main.py
@@ -9,6 +9,20 @@ def index():
 	return render_template('index.html')
 
 
+@app.route('/work/<project>')
+def work(project):
+	if (project == 'cognitive-services'):
+		return render_template('cognitive-services.html')
+	elif (project == 'image-processing'):
+		return render_template('image-processing.html')
+	elif (project == 'object-detection'):
+		return render_template('object-detection.html')
+	elif (project == 'raspberry-pi-lab'):
+		return render_template('raspberry-pi-lab.html')
+	elif (project == 'deep-learning-for-advanced-driver-assistance-system-applications'):
+		return render_template('deep-learning-for-advanced-driver-assistance-system-applications.html')
+
+
+
 if __name__ == '__main__':
 	app.run(host = '0.0.0.0', port = 80, debug = True)
-
