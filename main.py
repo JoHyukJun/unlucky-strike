a0fb2879a081434c9ae8b323fd91f059e9629fb4
Jo Hyuk Jun <johyukjun@Jo-MacBookPro.local>
2020-02-03 16:36:24 +0900

    email function update

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
+from flask_mail import Mail, Message
 import sys
 import os
 
 app = Flask(__name__)
+
+app.config.update(
+	DEBUG=True,
+	#EMAIL SETTINGS
+	MAIL_SERVER='smtp.gmail.com',
+	MAIL_PORT=465,
+	MAIL_USE_SSL=True,
+	MAIL_USERNAME = 'hyukzuny@gmail.com',
+	MAIL_PASSWORD = '#'
+	)
 
 @app.route('/')
 def index():
@@ -21,6 +32,42 @@ def work(project):
 		return render_template('raspberry-pi-lab.html')
 	elif (project == 'deep-learning-for-advanced-driver-assistance-system-applications'):
 		return render_template('deep-learning-for-advanced-driver-assistance-system-applications.html')
+
+
+@app.route('/email', methods=['POST', 'GET'])
+def send_email_button():
+	if request.method == 'POST':
+		first_name = request.form['fname']
+		last_name = request.form['lname']
+		sender_email = request.form['senderemail']
+		subject = request.form['subject']
+		message_content = request.form['message']
+
+
+		result = send_email(first_name, last_name, sender_email, subject, message_content)
+
+		if not result:
+			return redirect('/')
+		else:
+			return render_template('index.html', content="FAILED TO SEND EMAIL")
+	else:
+		return render_template('index.html',)
+
+
+
+def send_email(first_name, last_name, sender_email, in_subject, message_content):
+	try:
+		mail = Mail(app)
+		msg = Message(subject=in_subject, sender=sender_email, recipients=["computer@khu.ac.kr"])
+		#msg = Message(subject='test', sender="test@test.test", recipients=["computer@khu.ac.kr"])
+		msg.body = message_content
+		mail.send(msg)
+	except Exception:
+		print('email system error:')
+		pass
+	finally:
+		pass
+
 
 
 
