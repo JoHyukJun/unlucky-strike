0b1c61f72acc4e03a8ded3cfb40065c3f0fc82cf
Jo Hyuk Jun <computer@khu.ac.kr>
2020-12-25 14:32:38 +0900

    update naver seach advisor"
    "

---
+++ python-django/unluckystrike/service/owct.py
@@ -0,0 +1,38 @@
+import cv2
+import threading
+
+
+class WebCam(object):
+    def __init__(self):
+        self.video = cv2.VideoCapture(0)
+        (self.grabbed, self.frame) = self.video.read()
+        threading.Thread(target=self.update, args=()).start()
+
+
+    def __del__(self):
+        self.video.release()
+
+
+    def get_frame(self):
+        image = self.frame
+        ret, jpeg = cv2.imencode('.jpg', image)
+
+        return jpeg.tobytes()
+
+
+    def update(self):
+        while (True):
+            (self.grabbed, self.frame) = self.video.read()
+
+
+webcam = WebCam()
+
+
+def generate_frame(camera):
+    while (True):
+        frame = webcam.get_frame()
+
+        yield(b'--frame\r\n'
+        b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+
