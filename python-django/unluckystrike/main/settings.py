922c67cbd124c94aa8991492e390bcc1a3cb8b70
Jo Hyuk Jun <computer@khu.ac.kr>
2022-05-17 12:50:34 +0900

    rm line

--- python-django/unluckystrike/main/settings.py
+++ python-django/unluckystrike/main/settings.py
@@ -14,7 +14,6 @@ import os
 
 from pathlib import Path
 
-from . import secrets
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
