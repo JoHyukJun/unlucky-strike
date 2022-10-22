a324baff2b3ad455ecef2d5c1e12ce96852276ee
42decibel <hyukzuny@gmail.com>
2022-10-22 20:49:16 +0900

    feat: Delete static file

--- src/unluckystrike/main/settings.py
+++ src/unluckystrike/main/settings.py
@@ -146,16 +146,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    BASE_DIR / 'main/static',
-    BASE_DIR / 'projects/static',
-]
-STATIC_ROOT = os.path.join(BASE_DIR / 'static')
+STATIC_ROOT = os.path.join(BASE_DIR / '.static')
 
 # Media files
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR / 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR / '.media')
 
 
 # Ckeditor config
